9688d607e6ebfcf477d2fcde0a1b8695017d9d7b
lsiwh37249 <lsiwh37249@gmail.com>
2024-08-01 15:52:45 +0900

    make u.py

---
+++ src/mov_agg/u.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def merge():
+    read_df = pd.read_parquet('~/tmp/test_parquet')
+    cols = ['movieCd', #영화의 대표코드를 출력합니다.
+       'movieNm', #영화명(국문)을 출력합니다.
+        'openDt', #영화의 개봉일을 출력합니다.
+        'audiCnt', #해당일의 관객수를 출력합니다.
+        'load_dt', # 입수일자
+        'multiMovieYn', #다양성영화 유무
+        'repNationCd', #한국외국영화 유무
+       ]
+    df = read_df[cols]
+    print(df.head(3))
+    return df
+
+merge()
